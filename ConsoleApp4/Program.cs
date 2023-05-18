21205c1e63ba870b4b91b16c8fd0d39504f1b2cf
GrosulV <127082028+GrosulV@users.noreply.github.com>
2023-05-18 12:50:31 +0300

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача №1
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x}+{y}={x + y}");
+
+            //Задача №2
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.Write("z=");
+            //int z = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x}+{y}+{z}={x + y+ z}");
+
+            //Задача №3
+            //Console.Write("x=");
+            //double x = double.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //double y = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x}*{y}={x * y}");
+
+            //Задача №4
+            //Console.Write("x=");
+            //double x = double.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //double y = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x}/{y}={x / y:f1}");
+
+            //Задача №5
+            //Console.Write("a=");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x}+{y}={y}+{x}");
+
+            //Задача №6
+            //Console.Write("Как тебя зовут?");
+            //string x = (Console.ReadLine());
+            //Console.Write("Сколько тебе лет?");
+            //int y = int.Parse (Console.ReadLine());
+            //Console.WriteLine($"{x},ты родился в {2023-y}");
+
+            //Задача №7
+            //Console.Write("Номинал купюры=");
+            //int x = int.Parse (Console.ReadLine());
+            //Console.Write("Колличество купюр=");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Сумма денег = {x*y:c}");
+
+            //Задача №8
+            //Console.Write("Длина=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"p={4*a}");
+
+            //Задача №9
+            //Console.Write("Длина первой стороны=");
+            //int x = Convert.ToInt32 (Console.ReadLine());
+            //Console.Write("Длина второй стороны=");
+            //int y = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"p={2 * (x+y)}");
+
+            //Задача №10
+            Console.Write("R=");
+            int r = Convert.ToInt32 (Console.ReadLine());
+            const double pi = 3.14;
+            Console.WriteLine($"{2*pi*r}");
+
+        }
+    }
+}
